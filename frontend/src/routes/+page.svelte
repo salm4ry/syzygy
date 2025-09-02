3ed2c98c96b100eca50301c54f91587caeaedbe9
Salma Berriche <salma.berriche@gmail.com>
2025-09-02 16:13:18 +0100

    Add SvelteKit frontend
    - installed with npx sv create frontend
    - makes a GET request to /view to get the basic struct information: will
      use POST requests (user-supplied C code from a form) which the backend
      parses in future

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { onMount } from 'svelte';
+
+let data: JSON;
+
+onMount(async () => {
+	const response = await fetch('http://localhost:8000/view')
+	data = await response.json();
+	console.log("data: ", data);
+});
+</script>
+
+<h1>syzygy 🪐</h1>
+<!--<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to
+    read the documentation</p> -->
+
+<p style="font-family: monospace;">{JSON.stringify(data)}</p>
