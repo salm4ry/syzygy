15758ff2f6164ca02abcb4d9d02fb2f0e8a026ba
Salma Berriche <salma.berriche@gmail.com>
2025-09-08 22:46:31 +0100

    Set up navbar component, layout, and finalise theme
    - use catppuccin plugin for theme (macchiato)
    - make sticky navbar component
    - use +layout.svelte to put navbar on all routes and set padding for all
      children

---
+++ frontend/src/lib/Navbar.svelte
@@ -0,0 +1,18 @@
+<style>
+  /* stick to top of screen */
+  .sticky {
+      position: fixed !important;
+      top: 0 !important;
+      width: 100% !important;
+      z-index: 99 !important;
+  }
+</style>
+
+<!-- rounded-b: only round bottom -->
+<div class="navbar sticky bg-base-300 shadow-sm rounded-b">
+
+  <div class="flex-1">
+    <a class="btn btn-ghost text-xl" href="/">syzygy 🪐</a>
+  </div>
+
+</div>
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Navbar from "$lib/Navbar.svelte";
+
+  let { children } = $props();
+</script>
+
+<Navbar />
+
+<div class="m-20"></div>
+
+<div class="p-2">
+  {@render children()}
+</div>
