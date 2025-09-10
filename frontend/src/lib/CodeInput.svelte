f7ee2151a269340e3fef86d4dc225a1eee61407a
Salma Berriche <salma.berriche@gmail.com>
2025-09-10 22:48:59 +0100

    Add CodeInput component
    - code input box with bound value so that input can be captured by pages
      importing the component
    - set up adaptive layout for main page: two columns on medium/large
      screens and one on small

---
+++ frontend/src/lib/CodeInput.svelte
@@ -0,0 +1,18 @@
+<script>
+export let value;
+</script>
+
+<style>
+textarea {
+	min-height: 20rem;
+	field-sizing: content;
+	padding: 0.5em 1em;
+	font-family: monospace;
+}
+</style>
+
+<textarea type="text"
+	  placeholder="your code here"
+	  class="textarea bg-base-200 w-full h-full"
+	  bind:value>
+</textarea>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 import "../app.css";
+
 import { onMount } from 'svelte';
+import CodeInput from "$lib/CodeInput.svelte";
 
 let data: JSON;
+let code: string = "";
 
 onMount(async () => {
 	const response = await fetch('http://localhost:8000/view')
@@ -16,7 +19,29 @@ onMount(async () => {
     read the documentation</p> -->
 
 <!-- sample struct JSON information -->
+<!--
 <div class="mockup-code w-full">
 	<pre>{JSON.stringify(data, null, "\t")}</pre>
 </div>
+-->
 </article>
+
+<div class="w-90% h-90%">
+	<div class="grid md:grid-cols-2 sm:grid-cols-1 gap-4">
+		<div class="col-span-1">
+			<form method="POST">
+				<CodeInput bind:value={code}/>
+				<button class="btn btn-primary mt-4">
+					submit
+				</button>
+			</form>
+			<button class="btn btn-error mt-4"
+			onclick={() => console.log(code)}>
+				get code value
+			</button>
+		</div>
+		<div class="card bg-primary text-primary-content col-span-1">
+			<p class="m-2">image here</p>
+		</div>
+	</div>
+</div>
