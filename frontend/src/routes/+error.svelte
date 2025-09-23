d06ba4246822d75466fd61af145058237ddc80bb
Salma Berriche <salma.berriche@gmail.com>
2025-09-23 14:52:42 +0100

    Add custom error page
    show status code and message, include same styling, navbar etc.

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+import '../app.css';
+import { page } from '$app/state';
+</script>
+
+<article class="prose mb-4">
+	<h1><span class="text-primary">{page.status}</span> {page.error?.message}</h1>
+</article>
