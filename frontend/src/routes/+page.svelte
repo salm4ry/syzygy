6e635210da5389491ea2534062ef8fa84fe9141a
Salma Berriche <salma.berriche@gmail.com>
2025-09-29 22:59:14 +0100

    Reformat main component
    - bind code even though unused
    - remove text centering to match all other pages

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,16 +6,16 @@
 import "../app.css";
 
 import Syzygy from "$lib/Syzygy.svelte";
+
+let code: string;
 </script>
 
-<div class="center">
-<article class="prose mb-4">
+<article class="prose m-2 mb-4">
 <h1>welcome to syzygy 🪐</h1>
 
 <blockquote>
 In astronomy, a <strong class="text-primary">syzygy</strong> (/ˈsɪzədʒi/ SIZ-ə-jee; from Ancient Greek συζυγία (suzugía) 'union, yoking', expressing the sense of σύν (syn- "together") and ζυγ- (zug- "a yoke")) is a roughly straight-line configuration of three or more celestial bodies in a gravitational system.
 </blockquote>
 </article>
-</div>
 
-<Syzygy/>
+<Syzygy bind:code/>
