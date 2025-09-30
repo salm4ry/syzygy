0db6b75801525f2a1b50185c40c55efd51f1315f
Salma Berriche <salma.berriche@gmail.com>
2025-09-30 13:40:38 +0100

    Clean up multiline code examples
    use Javascript template literals (``)

--- frontend/src/routes/examples/examples.ts
+++ frontend/src/routes/examples/examples.ts
@@ -4,37 +4,37 @@ export const examples = [
 	description: 'Containing data types of different sizes (4, 2, and 1 bytes\
 		respectively)',
 	code:
-"struct letters {\n\
-	int a;\n\
-	short b;\n\
-	char c;\n\
-};",
+`struct letters {
+	int a;
+	short b;
+	char c;
+};`,
 }, {
 	name: 'Struct dependency',
 	description: 'Here, the second struct depends on the first i.e. uses the first\
 		within its definition.',
 	code:
-"typedef struct baz {\n\
-	char *name;\n\
-	int age;\n\
-	double money;\n\
-} baz_t;\n\
-\n\
-typedef struct baz_2 {\n\
-	char* name;\n\
-	char **double_name;\n\
-	struct baz before;\n\
-	int age;\n\
-	double money;\n\
-} baz_2_t;",
+`typedef struct baz {
+	char *name;
+	int age;
+	double money;
+} baz_t;
+
+typedef struct baz_2 {
+	char* name;
+	char **double_name;
+	struct baz before;
+	int age;
+	double money;
+} baz_2_t;`,
 }, {
 	name: 'Arrays',
 	description: 'syzygy can handle arrays as well!',
 	code:
-"struct fruits {\n\
-	int apple[4];\n\
-	bool banana[3];\n\
-	char cherry[2];\n\
-};"
+`struct fruits {
+	int apple[4];
+	bool banana[3];
+	char cherry[2];
+};`
 },
 ];
