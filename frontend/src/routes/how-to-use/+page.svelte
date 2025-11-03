cc083feeec1dd50d348a3220b927f7da3ec6de9c
Salma Berriche <salma.berriche@gmail.com>
2025-11-03 22:15:25 +0000

    Add typedef not supported warning to usage page
    syzygy does not support types defined with typedef: when using a struct
    that has been made into a type, replace its name with the struct name

--- frontend/src/routes/how-to-use/+page.svelte
+++ frontend/src/routes/how-to-use/+page.svelte
@@ -34,6 +34,15 @@ happen!</p>
 
 <h2>Supported Types</h2>
 
+<div role="alert" class="alert alert-warning alert-soft">
+<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+</svg>
+<div class="test">syzygy does <span class="font-bold">not</span> support types defined with <span
+class="code">typedef</span>: use the <span class="code">struct</span> name or
+original type name</div>
+</div>
+
 <p class="mb-2">
 	See the
 	<a href="https://docs.python.org/3/library/ctypes.html#fundamental-data-types"
